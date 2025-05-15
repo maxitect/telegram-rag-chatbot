c8083de4dfe080d611d2c0307547162725f021a5
maxitect <maxime.downe@gmail.com>
2025-05-15 11:36:02 +0100

    refactor: PEP 8 standards

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    MessageHandler,
+    filters,
+    ContextTypes,
+    CommandHandler,
+)
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 
-async def mirror_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def mirror(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message_text = update.message.text
     await update.message.reply_text(message_text)
 
@@ -17,5 +23,5 @@ async def code(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 app = ApplicationBuilder().token(os.getenv('TELEGRAM_CHATBOT_API_KEY')).build()
 app.add_handler(CommandHandler("code", code))
 app.add_handler(MessageHandler(
-    filters.TEXT & ~filters.COMMAND, mirror_message))
+    filters.TEXT & ~filters.COMMAND, mirror))
 app.run_polling()
