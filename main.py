3cde41769b3b56636b07959755e85b3113eafe26
maxitect <maxime.downe@gmail.com>
2025-05-15 11:31:27 +0100

    feat: /code slash command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes, CommandHandler
 from dotenv import load_dotenv
 import os
 
@@ -10,7 +10,12 @@ async def mirror_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     message_text = update.message.text
     await update.message.reply_text(message_text)
 
+
+async def code(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("https://github.com/maxitect/telegram-rag-chatbot")
+
 app = ApplicationBuilder().token(os.getenv('TELEGRAM_CHATBOT_API_KEY')).build()
+app.add_handler(CommandHandler("code", code))
 app.add_handler(MessageHandler(
     filters.TEXT & ~filters.COMMAND, mirror_message))
 app.run_polling()
